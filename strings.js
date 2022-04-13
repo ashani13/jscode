f93528444b498505a883dc022715652fd233a16c
ashani13 <ashani@rudrainnovative.in>
2022-04-13 14:48:17 +0530

    str() example function

---
+++ strings.js
@@ -0,0 +1,21 @@
+//  Strings 
+let myName = "Ashani"
+
+// using constructor
+let myAge  = new String("My age is 23");
+
+// console.log(myAge.length);
+
+//  escape() charactor
+//  use // for string
+
+//  indexOf 
+
+const myBioData = "Am ashani kapoor learning js";
+// console.log(myBioData.search("kapoor"));
+
+//  Extracting string parts
+
+//  slice() and it not include ends 
+let str = "Apple, Banana, Kiwi";
+console.log(str.slice(0, 5));
